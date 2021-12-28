5043b94468aaef8b81d6120d920daf99e8044d94
Mido-Hyuga <96544931+Mido-Hyuga@users.noreply.github.com>
2021-12-29 00:04:19 +0100

    Ex07_all_classes.java

--- Ex07_all_classes.java
+++ Ex07_all_classes.java
@@ -7,6 +7,9 @@ package controle_2;
 
 import java.text.SimpleDateFormat;
 import java.util.Date ;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -155,6 +158,75 @@ class Prime extends Article implements Produits
     }
 
    }
+   //la partie 4 
+   
+
+public class Magasin 
+{
+	Map<String, Double>		articles;
+	List<Article>			articlesVendus;
+	List<Article>			articlesStock;
+	
+	public Magasin() 
+	{
+		
+	}
+
+	public Magasin(Map<String, Double> articles, List<Article> articlesVendus, List<Article> articlesStock) 
+	{
+		this.articles = articles;
+		this.articlesVendus = articlesVendus;
+		this.articlesStock = articlesStock;
+	}
+	
+	public void vendreArticle(String nom, Double quantite)
+	{
+		boolean	trouve;
+		int		indice;
+		
+		trouve = false;
+		indice = -1;
+		if (this.articles.containsKey(nom))
+		{
+			for (int i = 0; i < articlesStock.size(); i++)
+			{
+				if (articlesStock.get(i).getNomProduit().equals(nom))
+				{
+					trouve = true;
+					indice = i;
+					break ;
+				}
+			}
+		}
+		if (trouve)
+		{
+			if (this.articles.get(nom) >= quantite)
+			{
+				this.articlesVendus.add(articlesStock.get(indice));
+				this.articles.put(nom, this.articles.get(nom) - quantite);
+				System.out.println("vende succse!");
+			}
+			else
+				System.out.println("vous avez que " + this.articles.get(nom) + " du " + nom);
+		}
+		else
+			System.out.println("produit introuvable !!");
+	}
+	
+	public void Approvisionner(String nom, Double quantite)
+	{
+		boolean trouve;
+		
+		trouve = false;
+		for (int i = 0; i < articlesStock.size(); i++)
+			if (articlesStock.get(i).getNomProduit().equals(nom))
+				trouve = true;
+		if (trouve)
+			articles.put(nom, quantite);
+		else 
+			System.out.println("produit introuvable");
+	}
+}
 class Main
 {
     public static void main(String[] args)
